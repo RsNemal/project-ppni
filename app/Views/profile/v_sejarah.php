ceb9c18671afce25f7889a7ef8a89b12e668fe50
= <=>
2025-07-09 23:45:49 +0800

    merapikan beberapa kode

--- app/Views/profile/v_sejarah.php
+++ app/Views/profile/v_sejarah.php
@@ -5,19 +5,27 @@
 
 <h1 class="text-center">Sejarah PPNI</h1>
 <div class="card">
-  <div class="card-body">
-  <p class="text-break">PPNI, singkatan dari Persatuan Perawat Nasional Indonesia, didirikan pada tanggal 17 maret 1974 melalui fusi berbagai organisasi perawat yang ada sebelumnya, untuk menjadi wadah tunggal profesi perawat di Indonesia. Tanggal pendirian ini kemudian ditetapkan sebagai hari parawat nasional.</p>
+    <div class="card-body">
+        <p class="text-break">PPNI, singkatan dari Persatuan Perawat Nasional Indonesia, didirikan pada tanggal 17 maret
+            1974 melalui fusi berbagai organisasi perawat yang ada sebelumnya, untuk menjadi wadah tunggal profesi
+            perawat di Indonesia. Tanggal pendirian ini kemudian ditetapkan sebagai hari parawat nasional.</p>
 
-  <p>Secara singkat sejarah PPNI adalah sebagai berikut :</p>
-  <ul class="text-justify">
-    <li>Fusi Organisasi :</li>
-    <p>PPNI lahir dari kebulatan tekad dan semangat yang sama dari para perintis perawat Indonesia untuk menyatukan tenaga keperawatan dalam satu wadah organisasi profesi yang kuat. Berbagai organisasi perawat saat itu melakukan fusi pada tanggal 17 Maret 1974 untuk membentuk PPNI</p>
-    <li>Tujuan dan Komitmen</li>
-    <p>PPNI berkomitmen untuk melindungi masyarakat dan profesi keperawatan, serta berupaya meningkatkan kesejahteraan anggotanya. Organisasi ini juga aktif memperjuangkan pengesahan RUU Keperawatan menjadi Undang-Undang untuk memberikan perlindungan hokum bagi profesi dan masyarakat.</p>
-    <li>Perkembangan dan Peran</li>
-    <p>Sejak didirikan, PPNI terus menata diri dan berkembang di berbagai wilayah di Indonesia. PPNI berperan penting dalam pengembangan Pendidikan tinggi keperawatan dan penerapan teknologi dalam praktik keperawatan serta advokasi profesi dan anggota.</p>
-  </ul>
-  </div>
+        <p>Secara singkat sejarah PPNI adalah sebagai berikut :</p>
+        <ul class="text-justify">
+            <li>Fusi Organisasi :</li>
+            <p>PPNI lahir dari kebulatan tekad dan semangat yang sama dari para perintis perawat Indonesia untuk
+                menyatukan tenaga keperawatan dalam satu wadah organisasi profesi yang kuat. Berbagai organisasi perawat
+                saat itu melakukan fusi pada tanggal 17 Maret 1974 untuk membentuk PPNI</p>
+            <li>Tujuan dan Komitmen</li>
+            <p>PPNI berkomitmen untuk melindungi masyarakat dan profesi keperawatan, serta berupaya meningkatkan
+                kesejahteraan anggotanya. Organisasi ini juga aktif memperjuangkan pengesahan RUU Keperawatan menjadi
+                Undang-Undang untuk memberikan perlindungan hokum bagi profesi dan masyarakat.</p>
+            <li>Perkembangan dan Peran</li>
+            <p>Sejak didirikan, PPNI terus menata diri dan berkembang di berbagai wilayah di Indonesia. PPNI berperan
+                penting dalam pengembangan Pendidikan tinggi keperawatan dan penerapan teknologi dalam praktik
+                keperawatan serta advokasi profesi dan anggota.</p>
+        </ul>
+    </div>
 </div>
 
 
